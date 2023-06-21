99f8c6b41590d527d8e98c72f3c7d94edc517937
Dina <73873857+dinywka@users.noreply.github.com>
2023-06-21 14:29:30 +0600

    cange/delete

--- main.py
+++ main.py
@@ -80,5 +80,83 @@ def view_create():
             print("MySQL connection is closed")
         return redirect(url_for('home'))
 
+@app.route("/change", methods=["GET", "POST"])
+def view_change():
+    if request.method == "GET":
+        pk = request.args.get('pk', default=0, type=int)
+        connection = mysql.connector.connect(
+            host='localhost',
+            user='dina',
+            password='28MySql!',
+            database='mydatabase'
+        )
+        try:
+            cursor = connection.cursor()
+            query = "SELECT id, description, datetime FROM posts WHERE id = %s"
+            values = (pk,)
+            cursor.execute(query, values)
+            row = cursor.fetchone()
+            if row:
+                post = {"id": row[0], "description": row[1], "datetime": row[2]}
+                return render_template('change.html', post=post)
+            else:
+                return redirect(url_for('home'))
+        except Error as e:
+            print("Error while fetching post from the 'posts' table:", e)
+        finally:
+            cursor.close()
+            connection.close()
+            print("MySQL connection is closed")
+    elif request.method == "POST":
+        pk = request.form.get("pk")
+        description = request.form.get("description")
+        date = datetime.datetime.now().strftime("%Y-%m-%d")
+        connection = mysql.connector.connect(
+            host='localhost',
+            user='dina',
+            password='28MySql!',
+            database='mydatabase'
+        )
+        try:
+            cursor = connection.cursor()
+            query = "UPDATE posts SET description = %s, datetime = %s WHERE id = %s"
+            values = (description, date, pk)
+            cursor.execute(query, values)
+            connection.commit()
+            print("Data updated successfully.")
+        except Error as e:
+            connection.rollback()
+            print("Error while updating data in the 'posts' table:", e)
+        finally:
+            cursor.close()
+            connection.close()
+            print("MySQL connection is closed")
+        return redirect(url_for('home'))
+
+@app.route("/delete", methods=["GET"])
+def view_delete():
+    pk = request.args.get('pk', default=0, type=int)
+    connection = mysql.connector.connect(
+        host='localhost',
+        user='dina',
+        password='28MySql!',
+        database='mydatabase'
+    )
+    try:
+        cursor = connection.cursor()
+        query = "DELETE FROM posts WHERE id = %s"
+        values = (pk,)
+        cursor.execute(query, values)
+        connection.commit()
+        print("Data deleted successfully.")
+    except Error as e:
+        connection.rollback()
+        print("Error while deleting data from the 'posts' table:", e)
+    finally:
+        cursor.close()
+        connection.close()
+        print("MySQL connection is closed")
+    return redirect(url_for('home'))
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8000, debug=True)
